2588a40c0989a8c3681dce262fcbf9277a912933
ElnurBDa <elnur.badalov.std@bhos.edu.az>
2023-09-11 21:16:00 +0400

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image  } from 'react-native';
+import Button from './components/button';
+
+const PlaceholderImage = require('./assets/background.png');
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      
+      <View style={styles.imageContainer}>
+        <Image source={PlaceholderImage} style={styles.image} />
+      </View>
+
+      <Text style={styles.baseText}>Some Cool App with buttons</Text>
+      
+      <View style={styles.footerContainer}>
+        <Button label="Button A" />
+        <Button label="Button B" />
+      </View>
+      
     </View>
   );
 }
@@ -13,8 +27,22 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  imageContainer: {
+    flex: 1,
+    paddingTop: 0,
+  },
+  baseText: {
+    color: '#fff',
+    fontSize: 30,
+    fontWeight: 'bold',
+    paddingBottom: 50
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
   },
 });
